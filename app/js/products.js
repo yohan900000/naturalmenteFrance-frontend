9fac7330ae94b44b4e537b563151afb933db503a
Protocol <72527796+yohan900000@users.noreply.github.com>
2023-11-08 10:52:10 +0100

    Refactor admin.html, products.js, main.scss, and _topBar.scss
    - Added a new section for managing the homepage in admin.html
    - Implemented functionality to open and close modals for clickable products in products.js
    - Adjusted styles for the main content section in main.scss
    - Modified width property and added width property to top_bar class in _topBar.scss

--- app/js/products.js
+++ app/js/products.js
@@ -1,29 +1,25 @@
-// Sélectionnez tous les éléments .clickable-product
 const clickableProducts = document.querySelectorAll('.clickable-product');
 
-// Ajoutez un gestionnaire d'événements à chaque élément .clickable-product
 clickableProducts.forEach((clickableProduct) => {
     const modalProduct = clickableProduct.querySelector('.modal-product');
     const closeButton = modalProduct.querySelector('.top-modal span');
+    const contentModal = modalProduct.querySelector('.content-modal');
 
     clickableProduct.addEventListener('click', (event) => {
         modalProduct.classList.add('modal-open');
-        event.stopPropagation(); // Empêche la propagation de l'événement pour éviter de déclencher le gestionnaire d'événements sur window
+        event.stopPropagation();
     });
 
     closeButton.addEventListener('click', (event) => {
-        event.stopPropagation(); // Empêche la propagation de l'événement
+        event.stopPropagation();
         modalProduct.classList.remove('modal-open');
     });
-});
 
-// Ajoutez un gestionnaire d'événements au niveau du document pour fermer le modal en dehors de .modal-product
-document.addEventListener('click', (event) => {
-    const modals = document.querySelectorAll('.modal-open');
-    modals.forEach((modal) => {
-        const modalContent = modal.querySelector('.modal-product');
-        if (!modalContent.contains(event.target)) {
-            modal.classList.remove('modal-open');
-        }
+    modalProduct.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+
+    modalProduct.addEventListener('click', (event) => {
+        modalProduct.classList.remove('modal-open');
     });
 });
